f78609bf662dcd9066321b50e6538fd76f0181c2
Валерия <@DESKTOP-89Q1D6N>
2022-01-31 12:12:56 +0200

    Weather Classes for DailyForecast plus city and SearchCity

---
+++ shop/shop/Models/Weather/SearchCity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.Models.Weather
+{
+    public class SearchCity
+    { 
+        [Required(ErrorMessage = "Enter City name!")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage ="Only text allowed!")]
+        [Display(Name = "City Name")]
+        public string CityName { get; set; }
+    }
+}
